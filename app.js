389d2f59db3e530209fbddf6f7338bdcbabb71b4
kelvinirungu <kelvins.irungu@gmail.com>
2022-07-06 18:49:53 +0300

    add blur effect & background

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const openButton = document.querySelector('.more-btn');
 const closeButton = document.querySelector('#close-btn');
-const popup = document.querySelector('.popup');
+const popup = document.querySelector('.popup-blur');
 
 
 const openPopup = (popup) => {
@@ -14,11 +14,11 @@ popup.classList.remove('active');
 }
 
 openButton.addEventListener('click', () => {
-const popup = document.querySelector('.popup');
+const popup = document.querySelector('.popup-blur');
 openPopup(popup);
 })
 
 closeButton.addEventListener('click', () => {
-const popup = document.querySelector('.popup');
+const popup = document.querySelector('.popup-blur');
 closePopup(popup);
 })
